ee75e1e9ef91bc68079ac5214fc2a73d6d08f15f
alluballu1 <45081751+alluballu1@users.noreply.github.com>
2022-02-01 00:52:23 +0200

    Update NavBar.jsx

--- pages/components/NavBar.jsx
+++ pages/components/NavBar.jsx
@@ -1,28 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Link from "next/link";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { StarFill, Stars } from "react-bootstrap-icons";
 const NavBar = () => {
   return (
-    <nav
-      style={{ padding: 20, background: "#301934", position:"fixed", zIndex:100000, width:"100%"}}
-      className="navbar navbar-expand-lg navbar-dark "
-    >
-      <a className="navbar-brand" href="#">
-        <Stars size={34} />
-      </a>
-      <a className="navbar-brand">TarotFlip</a>
-
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <Link href={"/"}>
-            <a className="nav-item nav-link">Home</a>
-          </Link>
-          <Link href={"/screens/TarotScreen"}>
-            <a className="nav-item nav-link">Table</a>
-          </Link>
-        </div>
-      </div>
-    </nav>
+    <Navbar style={{backgroundColor:"#301934 ", padding:15, position:"fixed", width:"100%", zIndex:100}} variant="dark"  bg="inherit" expand="lg">
+  <Container  >
+    <Navbar.Brand  href="/"><Stars size={35}/></Navbar.Brand>
+    <Navbar.Brand  href="/">TarotFlip</Navbar.Brand>
+    <Navbar.Toggle  aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link  href="/">Home</Nav.Link>
+        <Nav.Link  href="/screens/TarotScreen">Link</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
   );
 };
 export default NavBar;
