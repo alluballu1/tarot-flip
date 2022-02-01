9b7dd01e783ffe0573bd2d069d6ea11933f01d14
alluballu1 <45081751+alluballu1@users.noreply.github.com>
2022-02-02 00:03:08 +0200

    Update CardContainer.jsx

--- pages/components/CardContainer.jsx
+++ pages/components/CardContainer.jsx
@@ -1,7 +1,7 @@
 import { useDragControls, motion } from "framer-motion";
 import Image from "next/image";
 import React, { useState } from "react";
-
+import {ArrowRepeat} from "react-bootstrap-icons"
 const CardContainer = (props) => {
   const [rotation, setRotation] = useState(0)
   return (
@@ -14,12 +14,12 @@ const CardContainer = (props) => {
     >
       <motion.div
         style={{
-          width: 220,
-          height: 320,
+          width: 160,
+          height: 250,
           position: "absolute",
           zIndex: 10,
           background: "white",
-          padding: 25,
+          padding: 5,
           borderWidth:1,
           borderStyle:"solid",
           cursor:"move"
@@ -29,8 +29,12 @@ const CardContainer = (props) => {
         draggable
         animate={{ rotate: rotation }}
       >
-        <Image src={`/tarot/${props.card}`} width={200} height={300} />
-        <button onClick={() => setRotation(rotation + 90)}>Rotate</button>
+          <div style={{display:"flex", flexDirection:"row", justifyContent:"space-evenly"}}>
+          <p>{props.card}</p> <ArrowRepeat size={42} style={{background:"white",cursor:"pointer", position:"absolute", right:-15, top:-20, color:"black", borderRadius:"50%", borderStyle:"solid", borderWidth:1}} onClick={() => setRotation(rotation + 90)}/>
+          </div>
+          
+        <Image src={`/tarot/${props.card}`} width={150} height={200} />
+        
       </motion.div>
     </div>
   );
